9ca7e56ab6700a275835e77ed9b3d6bf24fd826a
MauricioRL15 <15rlmauricio@gmail.com>
2023-09-15 13:03:20 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+// Creado por: Mauricio Ramírez López
+// Asignatura: Desarrollo Móvil Integral
+// Grado y Grupo: 10°A
+// Docente: Ramírez Hernández Marco Antonio
+import 'package:flutter/material.dart';
