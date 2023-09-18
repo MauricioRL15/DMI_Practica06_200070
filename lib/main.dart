c4aeb3431dc6e06f48599c18d61f1e5ea0d0fce3
MauricioRL15 <15rlmauricio@gmail.com>
2023-09-18 00:26:52 -0600

    Ultimos cambios

--- lib/main.dart
+++ lib/main.dart
@@ -2,4 +2,9 @@
 // Asignatura: Desarrollo Móvil Integral
 // Grado y Grupo: 10°A
 // Docente: Ramírez Hernández Marco Antonio
+import 'package:dmi_practica06_200070/src/my_button.dart';
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: MyButton(), debugShowCheckedModeBanner: false));
+}
--- lib/src/my_button.dart
+++ lib/src/my_button.dart
@@ -1,1 +1,47 @@
 import 'package:flutter/material.dart';
+
+class MyButton extends StatefulWidget {
+  @override
+  _MyButtonState createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  String flutterText = "";
+  int index = 0;
+  List<String> collections = ['Hola', 'Mau', 'UTXJ'];
+
+  void onPressButton() {
+    setState(() {
+      flutterText = collections[index];
+      index = index < 2 ? index + 1 : 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:
+          AppBar(title: Text("STATEFUL WIDGET", textAlign: TextAlign.center)),
+      body: Container(
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(flutterText, style: TextStyle(fontSize: 40.0)),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+            ),
+            ElevatedButton(
+              child: Text(
+                "Update",
+                style: TextStyle(color: Colors.white),
+              ),
+              // color: Colors.blueAccent,
+              onPressed: onPressButton,
+            )
+          ],
+        )),
+      ),
+    );
+  }
+}
